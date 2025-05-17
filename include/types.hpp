12300c707475448795b00138039b75abb083ee7e
Mikhail Zykov <zmsmihail@yandex.ru>
2025-05-17 17:59:51 +0300

    Add fixed_string constructors

--- include/types.hpp
+++ include/types.hpp
@@ -1,12 +1,35 @@
 #pragma once
 
+#include <algorithm>
+
 namespace stdx::details {
 
 // Шаблонный класс, хранящий C-style строку фиксированной длины
 
-// ваш код здесь
+template<std::size_t N = 0>
 struct fixed_string {
-    // ваш код здесь
+    char data[N + 1]; // 1 для \0 в конце
+    constexpr fixed_string() = default;
+    constexpr fixed_string(const char (&str)[N]) {
+        std::copy(str, str + N, data);
+        data[N] = '\0';
+    }
+    template<std::size_t Size, typename = typename std::enable_if<(Size < N)>::type>
+    constexpr fixed_string(const char (&str)[Size]) {
+        std::copy(str, str + Size, data);
+        for (std::size_t i = Size; i < sizeof(data)/sizeof(char); ++i) {
+            data[i] = '\0';
+        }
+    }
+    constexpr fixed_string(char *begin, char *end) {
+        for (std::size_t i = 0; i < sizeof(data)/sizeof(char); ++i) {
+            if (begin != end) {
+                data[i] = *begin++;
+            } else {
+                data[i] = '\0';
+            }
+        }
+    }
 };
 
 // Шаблонный класс, хранящий fixed_string достаточной длины для хранения ошибки парсинга
